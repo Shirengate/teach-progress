48f34f738e6813a24d7f198e835a17453580b57f
Shirengate <sweetgames551ggg@gmail.com>
2025-06-27 21:21:45 +0300

    add test xhr

--- vue-project/src/views/HomePage.vue
+++ vue-project/src/views/HomePage.vue
@@ -9,7 +9,7 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import { useStateStore } from "@/stores/state";
 import { useFetchData } from "@/stores/fetchData";
 import PercentageBar from "@/components/HomePage/PercentageBar.vue";
@@ -28,8 +28,21 @@ async function getItems(): Promise<void> {
     console.error(e as string);
   }
 }
+
+async function fetchData(): Promise<void> {
+  const url: string =
+    "https://fetch-progress.anthum.com/30kbps/images/sunrise-baseline.jpg";
+  const xhr = new XMLHttpRequest();
+  xhr.onprogress = (event: ProgressEvent): void => {
+    const status: number = Math.floor((event.loaded / event.total) * 100);
+    console.log(status);
+  };
+  xhr.open("GET", url, true);
+  xhr.send();
+}
 onMounted(async () => {
   await getItems();
+  await fetchData();
 });
 </script>
 
